aa632f52aad0379ce97a80eebd5d9632de88d810
Klara <klara@MacBook-Air-de-test.local>
2026-09-15 14:55:36 +0200

    Added a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import HeroSection from "./components/HeroSection";
+import ContactHero from "./pages/ContactUsPage";
+import Navbar from "./pages/partial/Navbar";
 
 function App() {
   return (
-    <div>
-      <HeroSection />
-    </div>
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/about-us" element={<HeroSection />} />
+        <Route path="/contact" element={<ContactHero />} />
+      </Routes>
+    </>
   );
 }
 
