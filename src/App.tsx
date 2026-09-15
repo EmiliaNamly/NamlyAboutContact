a43d104b3a8f1f93da8dbd693de10e706c244bad
EmiliaNamly <emilia@namly.se>
2026-09-15 11:55:06 +0200

    Installed tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>Om oss</h1>
+      <h1 className="min-h-screen flex items-center justify-center text-4xl text-green-600">Om oss</h1>
     </div>
   );
 }
