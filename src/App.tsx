fdeeb7462bb0032cd419a99aede327604cbfb98e
EmiliaNamly <emilia@namly.se>
2026-09-22 13:18:50 +0200

    Added giftCardSection

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import ContactHero from "./pages/ContactUsPage";
 import Navbar from "./pages/partial/Navbar";
 import AboutUsPage from "./pages/AboutUsPage";
+import PresentPage from "./pages/PresentPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/about-us" element={<AboutUsPage />} />
         <Route path="/contact" element={<ContactHero />} />
+        <Route path="/presents" element={<PresentPage />} />
       </Routes>
     </>
   );
